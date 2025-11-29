6d1ea7d3a80afcab29a9489ed34ee9bb24d9646f
Vinicius Dos Santos Girotti <vinicius0621@hotmail.com>
2025-11-29 10:56:36 -0300

    Create funcoes.py
    Upando exercicios ja solucionados anteriormente para documentação/revisão/refinamento.

---
+++ Python/funcoes.py
@@ -0,0 +1,10 @@
+def somar(a, b):
+    return a + b
+
+def apresentar(nome, idade):
+    print(f"Olá {nome}, você tem {idade} anos!")
+
+resultado = somar(4, 9)
+print("Resultado:", resultado)
+
+apresentar("Ana", 20)
