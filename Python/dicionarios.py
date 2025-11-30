5aca5ed77d4d88f6bb0d79e9e41edf779f08c879
Vinicius Dos Santos Girotti <vinicius0621@hotmail.com>
2025-11-30 12:00:32 -0300

    Update dicionarios.py
    Criando estruturas condicionais para atender os critérios de cada pessoa e imprimir o nome de cada pessoa que se encaixa respectivamente.
    Juntamente com o algoritmo pensado passo a passo, interpretação do problema, para trazer solução e parâmetros de forma algoritmia para o console

--- Python/dicionarios.py
+++ Python/dicionarios.py
@@ -19,7 +19,7 @@ for chave, valor in cliente.items():
 
 ###########################################
 
-Perante a seguinte lista contendo dicionários, percorre cada condição imprimindo o nome e qual o critério de compra em que o cliente se encaixa:
+Perante a seguinte lista contendo dicionários, percorre cada item para que se encaixa na devida condição, imprimindo o nome e qual o critério de compra em que o cliente se encaixa:
 # DICIONÁRIO
 avaliacoes [
 {"NOME": "JOAO", "PAGAMENTO": True, "DESCONTO": "POSSUI"},
@@ -29,3 +29,21 @@ avaliacoes [
 ]
 
 for a in avaliacoes:
+ if a["PAGAMENTO"] == False and a["DESCONTO"] == "NAO POSSUI":
+   print(a["NOME"], "Está com a avaliação para aprovar compras baixas")
+   
+ elif a["PAGAMENTO"] == True and a["DESCONTO"] == "NAO POSSUI":
+   print(a["NOME"], "Está com a avaliação para aprovar compras moderadas")
+   
+ elif a["PAGAMENTO"] == False and a["DESCONTO"] == "POSSUI":
+   print(a["NOME"], "Está com a avaliação para aprovar compras medianas")
+   
+ else:
+   print(a["NOME"], "Está com a avaliação para aprovar compras altas")
+
+#####AlGORITMO PENSADO PASSO A PASSO QUE FOI PENSADO#########
+//uma lista de dicionarios foi concedida.
+//A questão pede que percorra cada item, para verificar cada estrutura condicional e imprimir o nome dentro da lista dicionário que se encaixa no devido critério.
+//Primeiro um atribuir a para a lista dicionario avaliacoes.
+//Segundo criar estruturas condicionais em que os critérios de cada cliente se encaixe, para que imprima resultado para todos.
+//Terceiro imprimir resultado com o nome do cliente que se encaixa em determinado critério com uma mensagem relacionada à sua permissão de compra.
